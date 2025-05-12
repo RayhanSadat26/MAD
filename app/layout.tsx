e86798ba33f7d8a9f900be2ae4aaf18e4097cca2
Rayhan <rayhansadat.cs23@bmsce.ac.in>
2025-05-12 12:56:21 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from "expo-router";
+import {useFonts} from "expo-font";
+import {UserDetailContext} from "./../context/UserDetail"
+import {useState} from "react";
+
+export default function RootLayout() {
+
+  useFonts({
+    'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf')
+  })
+  const [UserDetail,setUserDetail]=useState();
+  return(
+    <UserDetailContext.Provider value={{UserDetail,setUserDetail}}>
+    <Stack screenOptions={{headerShown: false}}>
+
+    </Stack>
+    </UserDetailContext.Provider>
+  )
+}
